e59535aba18fc8fe9759fd82282e21745748deb8
panica913@gmail.com <panica913@gmail.com>
2020-06-16 09:50:40 +0300

    добавление объекта

---
+++ js/main.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+
+// var getRandomIntInclusive = function (min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// var OFFER = {
+//   ammount: 8,
+//   author: {
+//     avatar: ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png']
+//   },
+//   offer: {
+//     title: ['Дворец', 'Замок', 'Бунгало'],
+//     address: {
+//       locationX: 600,
+//       locationY: 350
+//     },
+//     price: {
+//       min: 10000,
+//       max: 50000
+//     },
+//     type: ['place', 'flat', 'house', 'bungalo'],
+//     rooms: {
+//       min: 1,
+//       max: 3
+//     },
+//     guests: {
+//       min: 0,
+//       max: 2
+//     },
+//     checkin: ['12:00', '13:00', '14:00'],
+//     checkout: ['12:00', '13:00', '14:00'],
+//     features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+//     description: ['С балконом', 'Рядом метро'],
+//     photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+//   },
+//   location: {
+//     x: {
+//       min: 0,
+//       max: 1200
+//     },
+//     y: {
+//       min: 130,
+//       max: 630
+//     }
+//   }
+// };

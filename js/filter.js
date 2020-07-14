23727221a38c7b167a9aa4c53199d7c60dbd4ac4
panica913@gmail.com <panica913@gmail.com>
2020-07-14 18:26:12 +0300

    фильтр

--- js/filter.js
+++ js/filter.js
@@ -1,13 +1,31 @@
 'use strict';
 
-// (function () {
-//   var PINS_LIMIT = 5;
+(function () {
+  var PINS_LIMIT = 5;
 
-//   var filterForm = document.querySelectorAll('.map__filters');
-//   var filterItems = filter.querySelectorAll('select, input');
-//   var filterResult = [];
+  var filter = document.querySelector('.map__filters');
+  var filterItems = filter.querySelectorAll('select, input');
+  var typeSelect = filter.querySelector('#housing-type');
+  var priceSelect = filter.querySelector('#housing-price');
+  var roomsSelect = filter.querySelector('#housing-rooms');
+  var guestsSelect = filter.querySelector('#housing-guests');
+  var featuresFieldset = filter.querySelector('#housing-features');
+  var data = [];
+  var filteredData = [];
 
-//   var filter = function (element, index, array) {
+  var filterItem = function (it, item, key) {
+    return it.value === 'any' ? true : it.value === item[key].toString();
+  };
 
-//   };
-// })();
+  var filterByType = function (item) {
+    return filterItem(typeSelect, item.offer, 'type');
+  };
+
+  var filterByRooms = function (item) {
+    return filterItem(roomsSelect, item.offer, 'rooms');
+  };
+
+  var filterByGuests = function (item) {
+    return filterItem(guestsSelect, item.offer, 'guests');
+  };
+})();
